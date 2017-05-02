2603e7054b8fb2a2b01edc9d711c038039d6ef97
kiefersivitz <kmsivitz@gmail.com>
2017-05-01 20:03:34 -0400

    Fixing deploy python script

--- cleanup.py
+++ cleanup.py
@@ -3,4 +3,4 @@ import fileinput
 
 file = fileinput.FileInput('docs/index.html', inplace=True, backup='.bak')
 for line in file:
-    print(line.replace('</script', '<script '), end='')
+    print(line.replace('src="/', ' src="'), end='')
