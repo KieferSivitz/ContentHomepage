951a86595ca54dd19aadbd1b2a6e70a3ea0689f0
kiefersivitz <kmsivitz@gmail.com>
2017-05-11 15:27:49 -0400

    logging to test broken feature on gh-pages

--- src/components/twitchComponent.vue
+++ src/components/twitchComponent.vue
@@ -46,7 +46,7 @@ export default {
         window.addEventListener('resize', function () {
             let width = Number(document.getElementById('gridComponent1').getBoundingClientRect().width) - 40
             let height = width * (9 / 16)
-
+            console.log(width, height)
             document.getElementById('twitchPlayer').width = width
             document.getElementById('twitchPlayer').height = height
         })
