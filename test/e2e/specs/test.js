3735e209c2d977c37ba0a6589999c90ae01302e9
kiefersivitz <kmsivitz@gmail.com>
2017-05-24 15:12:53 -0400

    Adding e2e tests to verify that grid items are rendering

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,18 +2,21 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
+    'default e2e tests': function (browser) {
+        // automatically uses dev Server port from /config.index.js
+        // default: http://localhost:8080
+        // see nightwatch.conf.js
+        const devServer = browser.globals.devServerURL
 
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  }
+        browser
+            .url(devServer)
+            .waitForElementVisible('#app', 5000)
+            .assert.elementPresent('.gridComponent')
+            .assert.containsText('h1', 'Welcome to the Content Homepage!')
+            .assert.containsText('h3', 'Currently combining Twitch and Twitter for Super Smash Brothers Melee!')
+            .waitForElementVisible('#gridComponent0', 1000)
+            .waitForElementVisible('#gridComponent1', 1000)
+            .waitForElementVisible('#gridComponent2', 1000)
+            .end();
+    }
 }
---
+++ test/unit/specs/gridComponent.spec.js
@@ -0,0 +1,7 @@
+// import gridComponent from '../../../src/components/gridComponent'
+
+describe('gridComponent.vue', () => {
+    it('should return 9 when multiplying 3 by 3', () => {
+        expect(3 * 3).equal(9)
+    });
+})
