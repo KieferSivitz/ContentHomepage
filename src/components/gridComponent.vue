53cd87c7ad0d6e4bfb8e669f79997df921ed4e89
kiefersivitz <kmsivitz@gmail.com>
2017-06-08 21:37:28 -0400

    Adding Vuex and moving layout to be managed by Vuex

--- src/components/gridComponent.vue
+++ src/components/gridComponent.vue
@@ -50,38 +50,14 @@ export default {
     data () {
         return {
             msg: 'Welcome to the social media aggregator!',
-            layout: (() => {
-                let tmpLayout = [
-                    {'x': 0, 'y': 0, 'w': 3, 'h': 20, 'i': 'twitterComponent', 'id': 'gridComponent0'},
-                    {'x': 6, 'y': 0, 'w': 6, 'h': 12, 'i': 'twitchComponent', 'id': 'gridComponent1'},
-                    {'x': 3, 'y': 0, 'w': 3, 'h': 20, 'i': 'twitchChatComponent', 'id': 'gridComponent2'}
-                    // {'x': 6, 'y': 12, 'w': 6, 'h': 6, 'i': 'adsenseComponent', 'id': 'gridComponent3'}
-                ]
-                if (localStorage.getItem('layout')) {
-                    return JSON.parse(localStorage.getItem('layout'))
-                }
-                return tmpLayout
-            })()
-
+            layout: this.$store.state.gridLayout
         }
     },
 
     methods: {
-        resizeWithContainer: function (i, newH, newW, newWPx, newHPx, element, offsetW, offsetH) { // eslint-disable-line
+        resizeWithContainer: function (i, newH, newW, newWPx, newHPx, element, offsetW, offsetH) {
             let width = Number(newWPx) - offsetW
             let height = Number(newHPx) - offsetH
-
-            document.getElementById(element).width = width
-            document.getElementById(element).height = height
-        },
-
-        resizeWithContainerWidescreen: function (i, newH, newW, newWPx, newHPx, element, offsetW, offsetH) { // eslint-disable-line
-            let width = Number(newWPx) - 20
-            let height = width * (9 / 16)
-
-            if (height > Number(newHPx) - offsetH) {
-                height = Number(newHPx) - offsetH
-            }
 
             document.getElementById(element).width = width
             document.getElementById(element).height = height
@@ -92,7 +68,7 @@ export default {
             switch (i) {
             case 'twitchComponent':
                 this.storeItemProperties()
-                this.resizeWithContainerWidescreen(i, newH, newW, newWPx, newHPx, 'twitchPlayer', 70, 70)
+                this.resizeWithContainer(i, newH, newW, newWPx, newHPx, 'twitchPlayer', 20, 70)
                 break;
 
             case 'twitterComponent':
@@ -111,8 +87,7 @@ export default {
         },
 
         storeItemProperties: function () {
-            let layoutSaved = this.layout
-            localStorage.setItem('layout', JSON.stringify(layoutSaved))
+            this.$store.commit('saveLayout', this.layout)
         }
     }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import twitterComponent from './components/twitterComponent'
@@ -8,12 +6,49 @@ import gridComponent from './components/gridComponent'
 import twitchChatComponent from './components/twitchChatComponent'
 import adsenseComponent from './components/adsenseComponent'
 import router from './router'
+import Vuex from 'vuex'
+
+// Vuex State Management
+Vue.use(Vuex)
+
+// Check localstorage for a layout, otherwise use the default
+let tmpLayout = []
+if (!localStorage.getItem('layout')) {
+    tmpLayout = [{'x': 0, 'y': 0, 'w': 3, 'h': 20, 'i': 'twitterComponent', 'id': 'gridComponent0'},
+    {'x': 6, 'y': 0, 'w': 6, 'h': 12, 'i': 'twitchComponent', 'id': 'gridComponent1'},
+    {'x': 3, 'y': 0, 'w': 3, 'h': 20, 'i': 'twitchChatComponent', 'id': 'gridComponent2'}]
+    localStorage.setItem('layout', JSON.stringify(tmpLayout))
+} else {
+    tmpLayout = JSON.parse(localStorage.getItem('layout'))
+}
+
+const store = new Vuex.Store({
+    // State variables
+    state: {
+        count: 0,
+        gridLayout: tmpLayout
+    },
+    // State modification functions, behave like getters and setters would, accessing essentially private variables
+    mutations: {
+        increment (state) {
+            state.count++
+        },
+        saveLayout (state, currentLayout) {
+            let layoutSaved = currentLayout
+            localStorage.setItem('layout', JSON.stringify(layoutSaved))
+        }
+    }
+})
+store.commit('increment')
+// End of Vuex Code
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    store,
     router,
     template: '<App/>',
     components: {
